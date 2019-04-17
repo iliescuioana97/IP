fb8fab5dafaa39dd8adcedbd3086c1104492b4c6
Ioana Iliescu <ioanailiescu@iiliescu-macbook.am.ias.bitdefender.biz>
2019-04-17 19:56:14 +0300

    Added search functionality for movies page and movies pages

--- mycinema/movies/urls.py
+++ mycinema/movies/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='movies'),  # root path
     path('<int:movie_id>', views.movie, name='movie'),
+    path('search', views.search, name='search'),
 ]
--- mycinema/movies/views.py
+++ mycinema/movies/views.py
@@ -7,12 +7,12 @@ from .models import Movie
 def index(request):
     movies = Movie.objects.order_by('-date_added').filter(is_published=True)
 
-    # paginator = Paginator(movies, 3)
-    # page = request.GET.get('page')
-    # paged_movies = paginator.get_page(page)
-    #
+    paginator = Paginator(movies, 3)
+    page = request.GET.get('page')
+    paged_movies = paginator.get_page(page)
+
     context = {
-        'movies': movies
+        'movies': paged_movies
     }
 
     return render(request, 'movies/movies.html', context)
@@ -20,3 +20,20 @@ def index(request):
 
 def movie(request, movie_id):
     return render(request, 'movies/movie.html')
+
+
+def search(request):
+    queryset_movies = []
+    # Rooms
+    if 'movie' in request.GET:
+        print("HEREEEEEEEEEEEEEEEEEEEEEEEEEEEE")
+        room = request.GET['movie']
+        if room:
+            queryset_movies = Movie.objects.all().filter(is_published=True)
+            queryset_movies = queryset_movies.filter(name__icontains=room)  # column__iexact / column__exact
+
+    context = {
+        'movies': queryset_movies,
+        'values': request.GET
+    }
+    return render(request, 'movies/search.html', context)
