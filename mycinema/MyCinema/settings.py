ad47e4afe908998527370a572f69d80c1820632c
David VULTUR <dvultur@ias.bitdefender.biz>
2019-04-17 18:18:21 +0300

    Updated generic pw in db config

--- mycinema/MyCinema/settings.py
+++ mycinema/MyCinema/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'mycinema',
         'USER': 'postgres',
-        'PASSWORD': 'ius',
+        'PASSWORD': 'postgres',
         'HOST': 'localhost'
     }
 }
