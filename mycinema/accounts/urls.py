2e462da1954a21541c0cd00b5f61968725c94591
iustinadumitru <32546295+iustinadumitru@users.noreply.github.com>
2019-04-23 23:38:17 +0300

    minor fix

--- mycinema/accounts/urls.py
+++ mycinema/accounts/urls.py
@@ -7,5 +7,4 @@ urlpatterns = [
     path('register', views.register, name='register'),
     path('logout', views.logout, name='logout'),
     path('forgot_password', views.forgot_password, name='forgot_password'),
-    path('dashboard', views.dashboard, name='dashboard'),
 ]
