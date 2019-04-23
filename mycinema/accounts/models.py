a376be7cba5ec4a849763e8183f98abc31e15e8e
iustinadumitru <32546295+iustinadumitru@users.noreply.github.com>
2019-04-24 01:11:53 +0300

    added profile model and user listener

--- mycinema/accounts/models.py
+++ mycinema/accounts/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
+from phonenumber_field.modelfields import PhoneNumberField
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    photo = models.ImageField(upload_to='photos/users/%Y/%m/%d/', blank=True)
+    phone_number = PhoneNumberField(null=True, blank=True)
+    birthdate = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return self.user.user_name
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
