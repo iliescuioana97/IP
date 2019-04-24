78b3e2743854857c26699ff3813bdc96640a8fa0
David VULTUR <dvultur@ias.bitdefender.biz>
2019-04-24 18:49:48 +0300

    Fixed scripts

--- mycinema/mycinema/static/scripts/main.js
+++ mycinema/mycinema/static/scripts/main.js
@@ -1,3 +1,5 @@
-setTimeout(function() {
-  $('#message').fadeOut('slow');
-}, 2000);
+$(document).ready(function(){
+    setTimeout(function() {
+      $('#message').fadeOut('slow');
+    }, 2000);
+});
