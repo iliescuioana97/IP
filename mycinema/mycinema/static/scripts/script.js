ea327c98b08afc0ad2b1302da67e586694ea1686
David VULTUR <dvultur@ias.bitdefender.biz>
2019-04-24 20:08:41 +0300

    Settings page

--- mycinema/mycinema/static/scripts/script.js
+++ mycinema/mycinema/static/scripts/script.js
@@ -7,8 +7,31 @@ var setInitialTriggers = function() {
         e.preventDefault();
         $(".layout .l-left").toggleClass("on")
     })
+    $(".editProfileImage").on('click', function(e){
+        e.preventDefault();
+        $(".profile_image_upload_hidden").click()
+    })
+
+    $(".profile_image_upload_hidden").on('change', function() {
+        setProfImgSettings(this)
+    });
+    $(".settingsContainer .field label").on('click', function(){
+        $(this).parent().find("input").focus();
+    })
 }
 
 function getFocus() {
     document.getElementById("input_search").focus();
+}
+
+function setProfImgSettings(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function(e) {
+            $('.settingsContainer .image-part .profile').attr('src', e.target.result);
+        }
+
+        reader.readAsDataURL(input.files[0]);
+    }
 }
