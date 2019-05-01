bd6058c7422b8820a4595847bfdfab6015afd2a0
iliescuioana97 <iliescuioana97@gmail.com>
2019-05-01 15:37:59 +0300

    implemented new functions for statistics

--- mycinema/statistics_data/views.py
+++ mycinema/statistics_data/views.py
@@ -1,12 +1,18 @@
-from django.shortcuts import render, HttpResponse
-from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
-from django.contrib.auth.decorators import login_required
+from django.conf import settings
 from django.utils import timezone
+from django.shortcuts import render, HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.utils.timezone import make_aware
 from django.db.models import Min, Sum
 
 from program.models import Show, Ticket
+from accounts.models import Profile
+from movies.models import Movie
 
+import os
+import re
+import json
 import datetime
 
 
@@ -30,7 +36,21 @@ def index(request):
             'earnings_percentage': abs(admin_earnings['percentage']),
             'earnings_growth': True if admin_earnings['percentage'] > 0 else False
         }
+    age_distribution = statistics_age_distribution()
+    movie_distribution = statistics_movie_distribution()
 
+    statistics_location = os.path.join(settings.MEDIA_ROOT, 'statistics')
+    os.makedirs(statistics_location, exist_ok=True)
+
+    with open(os.path.join(statistics_location, 'age_distribution.json'), 'w+') as fh:
+        json.dump(age_distribution, fh, indent=4)
+
+    with open(os.path.join(statistics_location, 'movie_distribution.json'), 'w+') as fh:
+        json.dump(movie_distribution, fh, indent=4)
+
+    context['age_distribution_statistics'] = age_distribution
+    context['movie_distribution_statistics'] = movie_distribution
+    
     return render(request, 'statistics_data/statistics_data.html', context)
 
 
@@ -136,9 +156,95 @@ def statistics_admin_earnings():
     return data
 
 
+def statistics_age_distribution():
+    data = {'title': 'Age Distribution',
+            'param': 'Number of People',
+            'under 7': 0,
+            '7-14': 0,
+            '15-18': 0,
+            '19-24': 0,
+            '25–34': 0,
+            '35–44': 0,
+            '45–54': 0,
+            '55–64': 0,
+            '65+': 0
+            }
+    users = Profile.objects.filter(birthdate__isnull=False)
+
+    for user in users.iterator():
+        user_age = calculate_age(user.birthdate)
+        if user_age < 7:
+            data['under 7'] += 1
+        elif 7 <= user_age <= 14:
+            data['7-14'] += 1
+        elif 15 <= user_age <= 18:
+            data['15-18'] += 1
+        elif 19 <= user_age <= 24:
+            data['19-24'] += 1
+        elif 25 <= user_age <= 34:
+            data['25–34'] += 1
+        elif 35 <= user_age <= 44:
+            data['35–44'] += 1
+        elif 45 <= user_age <= 54:
+            data['45–54'] += 1
+        elif 55 <= user_age <= 64:
+            data['55–54'] += 1
+        elif user_age >= 65:
+            data['65+'] += 1
+
+    return data
+
+
+def statistics_movie_distribution():
+    data = {
+        'name': 'Movie Distribution',
+        'param': 'Number of Movies',
+        'Action': 0,
+        'Adventure': 0,
+        'Animation': 0,
+        'Biography': 0,
+        'Comedy': 0,
+        'Crime': 0,
+        'Documentary': 0,
+        'Drama': 0,
+        'Family': 0,
+        'Fantasy': 0,
+        'Film Noir': 0,
+        'History': 0,
+        'Horror': 0,
+        'Music': 0,
+        'Musical': 0,
+        'Mystery': 0,
+        'Romance': 0,
+        'Sci-Fi': 0,
+        'Short': 0,
+        'Sport': 0,
+        'Superhero': 0,
+        'Thriller': 0,
+        'War': 0,
+        'Western': 0
+    }
+
+    users = Movie.objects.all()
+
+    for movie in users.iterator():
+        genres = re.findall('\w+', movie.genre)
+        for genre in genres:
+            genre = genre.title()
+            if genre in data.keys():
+                data[genre] += 1
+
+    return data
+
+
 def human_format(num):
     magnitude = 0
     while abs(num) >= 1000:
         magnitude += 1
         num /= 1000.0
     return '%.2f%s' % (num, ['', 'K', 'M', 'G', 'T', 'P'][magnitude])
+
+
+def calculate_age(born):
+    today = datetime.date.today()
+    return today.year - born.year - ((today.month, today.day) < (born.month, born.day))
