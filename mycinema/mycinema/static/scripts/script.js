9915277430a350f5bdbfa4c506165d0c6e73b143
artur99 <david1989mail@yahoo.com>
2019-05-03 22:05:34 +0300

    Added program switch

--- mycinema/mycinema/static/scripts/script.js
+++ mycinema/mycinema/static/scripts/script.js
@@ -1,5 +1,10 @@
 $(document).ready(function(){
     setInitialTriggers();
+
+    $(".programs .buttons-top button").click(function (e) {
+       e.preventDefault();
+       $(this).removeClass('active');
+    });
 })
 
 var setInitialTriggers = function() {
